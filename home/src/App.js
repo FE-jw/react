737c2a93112975f24f709a76f193b98b32e83af9
jw <zaixu91@gmail.com>
2022-07-22 17:32:52 +0900

    배포 테스트

---
+++ home/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, nobis.<br />
+			안녕하세요.<br />
+			테스트 입니다. This is Test.
+		</div>
+	);
+};
+
+export default App;
