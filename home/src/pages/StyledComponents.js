e5ba579743feb624a1f0073654a10d3eceaa66a7
jw <zaixu91@gmail.com>
2022-09-07 22:04:32 +0900

    styled components 선언 위치 수정

--- home/src/pages/StyledComponents.js
+++ home/src/pages/StyledComponents.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledBtn = styled.button`
+	width:250px;
+	height:60px;
+	line-height:1.2;
+	font-size:1.6rem;
+	font-weight:bold;
+	color:#61dafb;
+	background-color:#282c34;
+
+	&:hover	{
+		border:1px solid #61dafb;
+	}
+
+	@media all and (min-width: 800px) {
+		width:400px;
+		height:80px;
+		font-size:2.0rem;
+	}
+`;
+
 const StyledComponents = () => {
-	const StyledButton = styled.button`
-		width:250px;
-		height:60px;
-		line-height:1.2;
-		font-size:1.6rem;
-		font-weight:bold;
-		color:#61dafb;
-		background-color:#282c34;
-
-		&:hover	{
-			border:1px solid #61dafb;
-		}
-
-		@media all and (min-width: 800px) {
-			width:400px;
-			height:80px;
-			font-size:2.0rem;
-		}
-	`;
 
 	return (
 		<div>
-			<StyledButton>Test Button</StyledButton>
+			<StyledBtn>Test Button</StyledBtn>
 		</div>
 	);
 };
