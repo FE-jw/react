f245b26df765e112214ae18ed7dccf7b9162da27
jw <zaixu91@gmail.com>
2022-09-23 22:26:14 +0900

    API 테스트(Lotto 지난 회차 정보 가져오기)

--- home/src/pages/Lotto.js
+++ home/src/pages/Lotto.js
@@ -54,9 +54,72 @@ const Result = ({ line }) => {
 	);
 };
 
+const SearchZone = () => {
+	const [lottoResult, setLottoResult] = useState();
+	const [resultFlag, setResultFlag] = useState(false);
+	const round = useRef();
+	
+	const search = lottoRound => {
+		let url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=' + lottoRound;
+
+		fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			if(data.returnValue === 'success'){
+				let lotto = [
+					data.drwNo,			//회차
+					data.drwNoDate,	//추첨 날짜
+					data.drwtNo1,	//첫번째 공
+					data.drwtNo2,	//두번째 공
+					data.drwtNo3,	//세번째 공
+					data.drwtNo4,	//네번째 공
+					data.drwtNo5,	//다섯번째 공
+					data.drwtNo6,	//여섯번째 공
+					data.bnusNo		//2등 보너스 공
+				];
+
+				setLottoResult(lotto);
+				setResultFlag(true);
+			}else if(data.returnValue === 'fail'){
+				alert('아직 추첨하지 않은 회차인가요? 값을 가져오지 못했어요.');
+			}
+		})
+		.catch((err) => {
+			alert('에러 발생! 잠시 후에 다시 시도해주세요.');
+		});
+	};
+
+	return(
+		<div>
+			<h2>지난 회차 번호 가져오기</h2>
+			<input
+				type="number"
+				placeholder="몇 회차"
+				ref={round}
+			/>
+			{<CommonBtn onClick={() => search(round.current.value)}>가져오기</CommonBtn>}
+			{
+				resultFlag && (
+					<>
+						<div>{lottoResult[0]} 회차 결과입니다.</div>
+						<div>{lottoResult[1]} 에 추첨했습니다.</div>
+						<div>첫번째 공은 <strong>{lottoResult[2]}</strong>입니다.</div>
+						<div>두번째 공은 <strong>{lottoResult[3]}</strong>입니다.</div>
+						<div>세번째 공은 <strong>{lottoResult[4]}</strong>입니다.</div>
+						<div>네번째 공은 <strong>{lottoResult[5]}</strong>입니다.</div>
+						<div>다섯번째 공은 <strong>{lottoResult[6]}</strong>입니다.</div>
+						<div>여섯번째 공은 <strong>{lottoResult[7]}</strong>입니다.</div>
+						<div>2등 보너스 공은 <strong>{lottoResult[8]}</strong>입니다.</div>
+					</>
+				)
+			}
+		</div>
+	);
+};
+
 const Lotto = () => {
 	const [isResult, setIsResult] = useState(false);
-	const lottoLine = useRef(0);
+	const lottoLine = useRef();
 
 	const buyLotto = (line) => {
 		if(line <= 0){
@@ -83,6 +146,8 @@ const Lotto = () => {
 			{
 				isResult && <Result line={lottoLine.current.value} />
 			}
+			<hr />
+			<SearchZone></SearchZone>
 		</div>
 	);
 };
